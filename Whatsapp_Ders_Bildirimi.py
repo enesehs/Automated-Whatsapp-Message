19fa85adb8801498cccf3d11ea5d6db34c26021d
Enes <129887158+enesehs@users.noreply.github.com>
2024-10-07 12:37:48 +0300

    whatsapp-automated-message-sender.py

--- Whatsapp_Ders_Bildirimi.py
+++ Whatsapp_Ders_Bildirimi.py
@@ -6,7 +6,7 @@ import time
 import schedule
 from datetime import datetime
 
-user_data_dir = "D:/Apps/WhatsApp_User_Data"
+user_data_dir = "D:/Apps/WhatsApp_User_Data" #Edit as you want
 
 def open_whatsapp():
     options = webdriver.ChromeOptions()
@@ -20,7 +20,7 @@ def open_whatsapp():
 
     driver = webdriver.Chrome(options=options)
     driver.get("https://web.whatsapp.com")
-    print("WhatsApp Web'e giriş Yapılıyor...")
+    print("Entering Whatsapp Web...")
     return driver
 
 def close_whatsapp(driver):
@@ -38,7 +38,7 @@ def send_message(group_name, message):
         search_box.clear()
         search_box.send_keys(group_name)
         time.sleep(3)
-        print("Grup Aranıyor...")
+        print("Searching Grup/User...")
         # Grubu seç
         group = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, f'//span[@title="{group_name}"]'))
@@ -52,42 +52,19 @@ def send_message(group_name, message):
         message_box.click()
         message_box.send_keys(message)
         time.sleep(1)
-        print("Ders Bildirimi Gönderiliyor...")
+        print("Sending Message...")
 
         send_button = driver.find_element(By.XPATH, '//button[@data-tab="11"]')
         send_button.click()
         time.sleep(2)
 
     except Exception as e:
-        print(f"Hata oluştu: {e}")
+        print(f"Error: {e}")
     finally:
         close_whatsapp(driver)
 
-schedule_list = [
-    {"day": "Monday", "time": "16:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Grafik-Animasyon. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=192620] "},
-    {"day": "Monday", "time": "19:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Ofis Yazılımları. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=192632] "},
-    {"day": "Tuesday", "time": "15:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! İngilizce. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=206591] "},
-    {"day": "Tuesday", "time": "17:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Türk Dili. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=205829] "},
-    {"day": "Tuesday", "time": "19:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Web Tasarım. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=192614] "},
-    {"day": "Wednesday", "time": "17:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Atatürk İnkılapları. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=191906] "},
-    {"day": "Wednesday", "time": "19:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Programlamanın Temelleri. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=192608] "},
-    {"day": "Thursday", "time": "15:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Yazılım Kurulumu. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=192626] "},
-    {"day": "Thursday", "time": "18:00", "group": "Ders Bildirimi", "message": "Ders Başlıyor! Matematik. Katılmayı Unutmayın! [https://debsis.firat.edu.tr/course/view.php?id=199724] "},
-]
+user_name = "Test" # Username/Number
+message_content = "Test [https://example.com]"  # Message
 
-for lesson in schedule_list:
-    if lesson["day"] == "Monday":
-        schedule.every().monday.at(lesson["time"]).do(send_message, lesson["group"], lesson["message"])
-    elif lesson["day"] == "Tuesday":
-        schedule.every().tuesday.at(lesson["time"]).do(send_message, lesson["group"], lesson["message"])
-    elif lesson["day"] == "Wednesday":
-        schedule.every().wednesday.at(lesson["time"]).do(send_message, lesson["group"], lesson["message"])
-    elif lesson["day"] == "Thursday":
-        schedule.every().thursday.at(lesson["time"]).do(send_message, lesson["group"], lesson["message"])
-
-
-while True:
-    schedule.run_pending()
-    now = datetime.now().strftime("%H:%M")
-    print(f"Saat: {now} | İşlem bekleniyor...")
-    time.sleep(60)
+if __name__ == "__main__":
+    send_message(user_name, message_content)
